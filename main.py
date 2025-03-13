b1b166a8c2fe3f4df4e55d815d7eea59e775595c
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2025-03-13 07:51:28 -0300

    feat : function reactor_efficiency is moved from meltdown_mitigation to main.py

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ def is_criticality_balanced(temperature, neutrons_emmited):
     else:
         return False
     
+
+def reactor_efficiency(voltage, current, theoretical_max_power):
+
+    generated_power  = voltage * current
+    efficiency = (generated_power/ theoretical_max_power)*100
+
+    if efficiency >= 80:
+        return 'green'
+    elif 60 <= efficiency< 80:
+        return 'orange'
+    elif 30 <= efficiency < 60:
+        return 'red'
+    else:
+        return 'black' 
+    
